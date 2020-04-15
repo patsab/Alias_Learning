da81ec2b27dda585e66b747efa916ec5ce5d5b51
psabau@LAPTOP-LMNGEJRN <sabau.pa@gmail.com>
2020-04-16 00:49:23 +0200

    Added Base structure of question window

--- aliasFrontend/src/app/components/dashboard/dashboard.component.ts
+++ aliasFrontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { Filter } from '../../models/Filter';
 
+//import the classes to know about the data structure
+import { Filter,Thema } from '../../models/Filter';
+
+//the Service is imported and injected, so the data can be retrieved from it
 import { FilterService } from  '../../services/filter.service'
 import { Router } from '@angular/router';
 
@@ -16,25 +18,33 @@ import { Router } from '@angular/router';
 
 export class DashboardComponent implements OnInit {
   
+  //Data of the class
   filters:Filter[];
+  
+  //the service and other stuff is injected, so it can be used inside the class
+  constructor(private breakpointObserver: BreakpointObserver
+    ,private filterService: FilterService
+    ,private router: Router) {}
+  
 
+  //ngOnInit is triggered after the component is opened, so basically at the creation of the component
+  ngOnInit(){
+    //if the user hasn't a valid session, he will be routed to the login page
+    if (!sessionStorage.getItem('email')){
+      this.router.navigate(['/login'])
+    }
+
+    //get the filters for the user to show these in the frontend
+    this.filterService.getFilterfromBackend(sessionStorage.getItem('email'))
+        .subscribe(filters => {this.filters=filters})
+  }
+
+
+  //this is true if the user device is a smartphone,otherwise it is false
+  //this is used for the responsive design
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
   );
-  
-  constructor(private breakpointObserver: BreakpointObserver
-    ,private filterService: FilterService
-    ,private router: Router) {}
-    
-  ngOnInit(){
-    if (!sessionStorage.getItem('email')){
-      this.router.navigate(['/login'])
-    }
-
-    this.filterService.getFilterfromBackend(sessionStorage.getItem('email'))
-        .subscribe(filters => {this.filters=filters})
-  }
-
 }
